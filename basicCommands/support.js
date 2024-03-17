0ef3841d46cc909afc320269473cb8cbd6ecb58c
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-17 20:05:10 +0200

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,13 +4,19 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = 'https://discord.gg/FUEHs7RCqz';
-        const embed = new EmbedBuilder()
-            .setColor('#FFFFFF')
-            .setTitle('Support server')
-            .setDescription(`Click [here](${supportServerLink}) to join our server.\nWe will be there for you anytime ❤️`)
-            .setThumbnail(`https://cdn.discordapp.com/attachments/1113800537402527903/1119995338359586837/a_e8d3128cee7f07ccec68962a8294f43d.gif`)
-            .setTimestamp();
+              const discordServerLink = 'https://discord.gg/XuvB8URUez';
+      const discordProfileLink = 'https://discordapp.com/users/1126336222206365696';
+
+      const embed = new EmbedBuilder()
+        .setColor('#ffffff')
+        .setAuthor({
+          name: 'Support',
+        })
+        .setDescription(`\nNeed help? No worries, we are here.\n
+Support Server: [Join Here](${discordServerLink})\n
+Developer Profile: [Carl (Owner)](${discordProfileLink})`)
+    
+
 
 
         message.reply({ embeds: [embed] });
