15f42111247afdc7c571c809b12f2945ffdd7cfb
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-17 21:11:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Update Out V@1.2`, type: ActivityType.Watching }],
+      activities: [{ name: `Luffy Conquer ⚔️`, type: ActivityType.Watching }],
       status: 'idle',
     });
   }, 2000); 
